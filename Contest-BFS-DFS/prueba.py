259b0b753d79f9abda977e31c7face15568bc09a
jpcortesg <juan2001cortes.jc@gmail.com>
2020-09-01 18:16:05 -0500

    change in folders

--- Contest-BFS-DFS/prueba.py
+++
@@ -1,9 +0,0 @@
-def rec(uno, dos, mayor):
-    if(uno > dos):
-        return mayor
-    else:
-        mayor += dos
-        return rec(uno, dos-1, mayor)
-
-algo = rec(3, 8, 0)
-print(algo)
---
+++ Lessons/C++/helloWorld.cpp
@@ -0,0 +1,10 @@
+#include <iostream> // Data input and output
+
+using namespace std; // For print
+
+int main(){
+
+  cout << "Hello World!!";
+
+  return 0;
+}
